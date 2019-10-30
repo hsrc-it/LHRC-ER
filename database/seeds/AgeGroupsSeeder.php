a15d87ba54816d3c41d912655d52265a793bf015
wejdan <hsrc-it@pnu.edu.sa>
2019-10-30 10:01:46 +0300

    remove agegroups update migration

--- database/seeds/AgeGroupsSeeder.php
+++ database/seeds/AgeGroupsSeeder.php
@@ -16,22 +16,22 @@ class AgeGroupsSeeder extends Seeder
       $ageGroups = array(
           array(
             'englsih_name' => 'Child [0-12]',
-            'arabic_name' => '',
+            'arabic_name' => 'أطفال [0-12]',
             'created_at' => today()
           ),
           array(
             'englsih_name' => 'Adolescent [13-18]',
-            'arabic_name' => '',
+            'arabic_name' => 'مراهقين[13-18]',
             'created_at' => today()
           ),
           array(
             'englsih_name' => 'Adults [19-64]',
-            'arabic_name' => '',
+            'arabic_name' => 'بالغين [19-64]',
             'created_at' => today()
           ),
           array(
             'englsih_name' => 'Elderly [65+]',
-            'arabic_name' => '',
+            'arabic_name' => 'كبار السن [+65]',
             'created_at' => today()
           )
         );
